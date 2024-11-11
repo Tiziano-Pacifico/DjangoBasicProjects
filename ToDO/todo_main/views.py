8d0d570ce35d64e7e53d3f4a2e17853907f2ce56
Tiziano Pacifico <gourmetmolise@gmail.com>
2024-11-11 14:39:09 +0100

    Add new features

--- ToDO/todo_main/views.py
+++ ToDO/todo_main/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from todos.models import Task
+
 def home(request):
-    return render(request, 'home-todo.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    print(tasks)
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'home-todo.html', context)
+
